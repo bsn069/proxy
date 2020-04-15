d076a8b22464b53ccfcd10b8a79e72404ee059af
bsn <513026809@qq.com>
2020-04-15 16:48:53 +0800

    aa

--- sync_to_src_rep.sh
+++ sync_to_src_rep.sh
@@ -2,10 +2,12 @@
  
 echo 在备份库修改后，需要执行此脚本 将修改同步到原仓库
 
-
+rm -rf src_rep
 git clone git@gitee.com:bsn/proxy.git src_rep
 
+echo 拉取最新的原仓库
 pushd src_rep
+git pull
 git checkout .
 git clean -fdx
 git pull
@@ -13,9 +15,20 @@ popd
 
 echo 删除原仓库所有文件
 rm -rf src_rep/*
-rm -rf src_rep/.
+rm -rf src_rep/.gitmodules
+rm -rf src_rep/.travis.yml
 
 echo 将当前仓库内容打包
+rm -rf src_rep.tar
 git archive --format tar --output src_rep.tar master
 echo 将当前仓库解包到原仓库
 tar -xf src_rep.tar -C src_rep
+
+echo 上传最新的原仓库
+pushd src_rep
+git submodule init
+git submodule update
+git add .
+git commit -m "auto sync to src rep"
+git push
+popd

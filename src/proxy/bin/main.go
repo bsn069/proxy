8aec5fa3ba252dd20234ac4ef8c05928f7463cae
bsn <513026809@qq.com>
2020-04-20 20:59:25 +0800

    aa

---
+++ src/proxy/bin/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"proxy/socks5"
+)
+
+func main() {
+	fmt.Println(1)
+
+	conf := &socks5.Config{}
+	server, err := socks5.New(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create SOCKS5 proxy on localhost port 8000
+	if err := server.ListenAndServe("tcp", "127.0.0.1:8000"); err != nil {
+		panic(err)
+	}
+}
